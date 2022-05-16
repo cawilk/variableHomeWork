020835d12bdb93116f495e07007d8c267aca8648
Carl <wilkinson.carl123@gmail.com>
2022-05-16 19:47:29 -0400

    vaiableHomeWork

---
+++ index.js
@@ -0,0 +1,89 @@
+// var money = 10;{
+//   money + 50;  
+// }
+// console.log(money)
+
+// var money = 10;{
+//  money + 45    
+// }
+
+// var money = 10;
+// var money = 10;
+// var money = 10;
+// var money = 10;
+// var money = 10;
+// var money = 10;
+// var money = 10;
+// var money = 10;{
+//     money ++
+//     console.log(money)
+// }
+
+// var money = 10;{
+//     money ++ ;
+//   console.log(money)  
+// }
+
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i = 9;
+var j = 10;
+
+
+console.log(a+b);
+console.log(c+d);
+console.log(e-f)
+console.log(g-h)
+console.log(i*j)
+console.log(g*h)
+console.log(d/j)
+console.log(a/b);
+console.log(++a);
+console.log(++a);
+console.log(--b);
+console.log(--a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
